4360099980bc2bdef50843386459df96ecc6f07c
aarnav singh <aarnavsingh836@gmail.com>
2023-10-25 18:47:14 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function qrGenerate() {
+	let inputBox = document.querySelector(".inputBox");
+	let qrImg = document.querySelector(".qr");
+	let download = document.querySelector("#download-qr");
+
+	qrImg.style.display = "block";
+
+	let randomQuote = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputBox.value}`;
+
+	if (inputBox.value.length == 0) {
+		alert("enter text or url");
+	} else {
+		qrImg.src = randomQuote;
+		qrImg.setAttribute("title", inputBox.value);
+	}
+}
